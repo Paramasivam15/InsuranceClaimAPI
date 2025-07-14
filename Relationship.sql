348d5cd0ab79e01a7e01aa8cf0aa0fe8587df793
Paramasivam15 <37594404+Paramasivam15@users.noreply.github.com>
2025-07-14 17:33:28 +0530

    Add files via upload

---
+++ Relationship.sql
@@ -0,0 +1,36 @@
+USE [ActivPatient]
+GO
+
+INSERT INTO [dbo].[RelationshipType]
+           ([Type])
+     VALUES
+           ('Self')
+INSERT INTO [dbo].[RelationshipType]
+           ([Type])
+     VALUES
+           ('Spouse')
+INSERT INTO [dbo].[RelationshipType]
+           ([Type])
+     VALUES
+           ('Son')
+INSERT INTO [dbo].[RelationshipType]
+           ([Type])
+     VALUES
+           ('Daughter')
+INSERT INTO [dbo].[RelationshipType]
+           ([Type])
+     VALUES
+           ('Mom')
+INSERT INTO [dbo].[RelationshipType]
+           ([Type])
+     VALUES
+           ('Dad')
+INSERT INTO [dbo].[RelationshipType]
+           ([Type])
+     VALUES
+           ('MotherInLaw')
+INSERT INTO [dbo].[RelationshipType]
+           ([Type])
+     VALUES
+           ('FatherInLaw')
+GO
